3c3eb6d3817a6d6c16ef6ef671f7b39c9db1da7e
george pang <xilvar@yahoo.com>
2017-04-03 01:50:53 -0700

    estimate eta

--- sockburn.py
+++ sockburn.py
@@ -5,7 +5,7 @@ import argparse
 import socket
 import ometer
 
-ITERATIONS = 1024000
+ITERATIONS = 102400
 BUFSIZE = 102400
 
 PORT = 6666
@@ -16,7 +16,7 @@ def send(host, port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((host, port))
     buf = BUFSIZE * "\0"
-    om = ometer.OMeter("MB")
+    om = ometer.OMeter("MB", ITERATIONS*BUFSIZE/1024.0/1024.0)
     for i in xrange(ITERATIONS):
         s.sendall(buf)
         om.iteration(BUFSIZE/1024.0/1024.0)
@@ -28,13 +28,12 @@ def receive(port):
     s.listen(1)
     total = 0
     conn, addr = s.accept()
-    om = ometer.OMeter("MB")
+    om = ometer.OMeter("MB", ITERATIONS*BUFSIZE)
     while total < ITERATIONS*BUFSIZE:
         data = conn.recv(BUFSIZE)
         actual = len(data)
         total += actual
         om.iteration(actual/1024.0/1024.0)
-    print total
     conn.close()
 
 def run():
@@ -44,8 +43,6 @@ def run():
     p.add_argument('--port', default=PORT)
 
     ns = p.parse_args()
-
-    print ns
     if ns.mode == "s":
         send(ns.host, ns.port)
     elif ns.mode == "r":
